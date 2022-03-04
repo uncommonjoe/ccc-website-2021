206244864e45c94a29d2321874b27f547687b5a8
Joe McLean <uncommonjoe@gmail.com>
2022-03-04 16:00:36 -0700

    Update UI to set live

--- set-live.php
+++ set-live.php
@@ -27,18 +27,69 @@
 		
 		//$success = $wpdb->insert( $table, $data );
 		//$success = $wpdb->query("UPDATE $table SET live = '$isLive' WHERE id = 1"));
-
+		
 		$success = $wpdb->update(
 			'wp_live', 
 			array('live' => $isLive),
 			array('id'=>1)
 		);
+
+		$title = null;
+		$hasTitle = null;
 		
-        if(!$success){
+		if(!$success){
 			$wpdb->print_error();
 		}
 		else {
-			echo 'Success';
+			
+			if($isLive === "true"){
+				$title = 'Now live!';
+				$hasTitle = true;
+			}
+
+			else if($isLive === "false") {
+				$title = 'Live is off';
+				$hasTitle = true;
+			}
+			
 		}
 	}  
 ?>
+<html>
+
+<body>
+    <section>
+        <div>
+            <img src="../wp-content/themes/ccc-website-2021/img/logo-header.svg" />
+            <h1><?php echo $title; ?></h1>
+            <h1><?php if(!$hasTitle && !$isLive) { echo "Enter parameter in URL"; } ?></h1>
+        </div>
+    </section>
+</body>
+
+<style type="text/css">
+body {
+    font-family: Montserrat, -apple-system, "Segoe UI", "Open Sans", "Helvetica Neue", sans-serif;
+}
+
+section {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 90vh;
+}
+
+section>div {
+    text-align: center;
+}
+
+img {
+    width: 50vw;
+}
+
+h1 {
+    font-size: 4vw;
+}
+</style>
+
+</html>
